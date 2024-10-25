0d0a7788bbe33cd7a4648a2557ef4d9c7368da21
dangtong76 <dangtong@gmail.com>
2024-10-26 01:16:30 +0900

    add gitignore

--- makeENV/create_docker_volume.sh
+++ makeENV/create_docker_volume.sh
@@ -1,7 +1,11 @@
 # 볼륨 새성 (volume name : cloud-cicd-src)
 docker volume create  \
---opt device="/Users/dangtongbyun/Library/CloudStorage/GoogleDrive-dbyun@redhat.com/My Drive/05.Lecture/CICD/cloud-cicd/src" \
+--opt device="/Users/dangtongbyun/Library/CloudStorage/GoogleDrive-dbyun@redhat.com/내 드라이브/05.Lecture/cicd/cloud-cicd/src" \
 --opt o=bind --opt type=none cloud-cicd-src
+
+docker volume create  \
+--opt device="/Users/dangtongbyun/Library/CloudStorage/GoogleDrive-dbyun@redhat.com/내 드라이브/05.Lecture/cicd/cloud-cicd/vscode" \
+--opt o=bind --opt type=none vscode
 
 # 볼륨 생성 확인
 docker volume ls
